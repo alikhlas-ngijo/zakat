84a4971f6c0215ce94bda8f843f4a78c0d11880d
alikhlas-ngijo <alikhlas.ngijo@gmail.com>
2026-03-13 11:06:22 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // config.js
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbxDcbnvGndDGkCJsHso9QIqi14leMwxFz_DyrMgn-25Fz5SVi-SzdjuQW2WpzFbf5zU/exec' // Ganti dengan URL Apps Script Anda
+    API_URL: 'https://script.google.com/macros/s/AKfycbwjKCHYtAz9thk2KD-vDHuPbGP37ApQr8PgG-iPgVAF-NIo4cKw63jV4hk5Tj5KRTLB/exec' // Ganti dengan URL Apps Script Anda
 };
