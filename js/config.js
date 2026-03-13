ffa850f3ccf67b301bcdd4f9b7bcbff9c87258fb
alikhlas-ngijo <alikhlas.ngijo@gmail.com>
2026-03-13 15:29:50 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // config.js
 var CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbwdhlXSVYCyLOxbnEyqATbjD0FGLDceVkzVxSdrELcfiNhDHtTjEthXkwJ8Jd1CuyS1/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbw2FMgpixcnF68aUfOn1GWhh7d2b-NSJG4jE3DBITc7n5VBc8FsvRloE7soIZM0nIv9/exec'
 };
